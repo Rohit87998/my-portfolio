73511e50a34895936ffcde5ab24b984ea9a3db9e
Rohit87998 <phatak.rohit.temeb27@gmail.com>
2023-09-05 02:08:18 +0530

    1st

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".stack-links");
+    const icon = document.querySelector(".stack-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
